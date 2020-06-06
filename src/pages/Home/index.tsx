819e8b371bdd7782e9474d17dcd4ac491cf525c8
Mickael Araujo <araujomickael18@gmail.com>
2020-06-06 06:17:18 -0300

    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Feather as Icon } from '@expo/vector-icons'
+import {
+  ImageBackground, View, Text,
+  Image
+} from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import styles from './styles'
 
 const Home = () => {
   return (
-    <View />
+    <ImageBackground
+    style={styles.container}
+    source={require('../../assets/home-background.png')}
+    imageStyle={{ width: 274, height: 368 }}
+    >
+
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')}/>
+
+        <Text style={styles.title}>
+          Seu marketplace de coleta de resíduos
+        </Text>
+
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton 
+        style={styles.button}
+        onPress={() => {}}
+        >
+          <View style={styles.buttonIcon}>
+            <Icon name='log-in' color='#FFF' size={26} />
+          </View>
+
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+
+    </ImageBackground>
   )
 }
 
